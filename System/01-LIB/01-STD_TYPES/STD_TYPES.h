946ef7bf1641e0208b6a5084d6b1b2acf74cb77b
Muhammad Ali Moutiry <moutiry4@gmail.com>
2020-08-20 20:24:40 +0200

    Project added to master branch

---
+++ System/01-LIB/01-STD_TYPES/STD_TYPES.h
@@ -0,0 +1,14 @@
+/***************************************************************************************************/
+/** Author    : Muhammad Ali Moutiry                                                               */
+/** Date      : 20 August 2020                                                                     */
+/** Version   : V01                                                                                */
+/***************************************************************************************************/
+#ifndef STD_TYPES_H
+#define STD_TYPES_H
+
+typedef unsigned char        u8 ;
+typedef unsigned short int   u16;
+typedef unsigned long  int   u32; 
+
+
+#endif
---
+++ System/01-LIB/02-BIT_MATH/BIT_MATH.h
@@ -0,0 +1,15 @@
+/***************************************************************************************************/
+/** Author    : Muhammad Ali Moutiry                                                               */
+/** Date      : 20 August 2020                                                                     */
+/** Version   : V01                                                                                */
+/***************************************************************************************************/
+#ifndef BIT_MATH_H
+#define BIT_MATH_H
+
+#define SET_BIT(VAR,BIT)          VAR |=  (1 << (BIT))
+#define CLR_BIT(VAR,BIT)          VAR &= ~(1 << (BIT)) 
+#define GET_BIT(VAR,BIT)          ((VAR >> BIT) & 1  )
+#define TOG_BIT(VAR,BIT)          VAR ^=  (1 << (BIT))
+
+
+#endif 
