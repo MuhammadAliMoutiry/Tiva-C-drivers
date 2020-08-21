9d68f38916a3436fe3f8d0d85506fac06d4be1de
Muhammad Ali Moutiry <moutiry4@gmail.com>
2020-08-21 10:30:02 +0200

    SYSCTRL enable GPIO clock function is added and tested. SYSCTRL disable GPIO clock function is added and tested. Test code exists in the main.c file.

--- System/02-MCAL/01-SYSCTRL/SYSCTRL_private.h
+++ System/02-MCAL/01-SYSCTRL/SYSCTRL_private.h
@@ -7,6 +7,13 @@
 #ifndef SYSCTRL_PRIVATE_H_
 #define SYSCTRL_PRIVATE_H_
 
+#define SYSCTRL_GPIO        0
+#define SYSCTRL_GPIOA_ID    0
+#define SYSCTRL_GPIOB_ID    1
+#define SYSCTRL_GPIOC_ID    2
+#define SYSCTRL_GPIOD_ID    3
+#define SYSCTRL_GPIOE_ID    4
+#define SYSCTRL_GPIOF_ID    5
 
 
 #endif /* SYSCTRL_PRIVATE_H_ */
--- System/02-MCAL/01-SYSCTRL/SYSCTRL_program.c
+++ System/02-MCAL/01-SYSCTRL/SYSCTRL_program.c
@@ -4,6 +4,24 @@
 /** Version   : V01                                                                                */
 /***************************************************************************************************/
 
+#include "SYSCTRL_interface.h"
+
+
+void SYSCTRL_voidInitSysClock(void){
+
+}
+
+void SYSCTRL_voidEnableClock(u8 Copy_u8Peripheral , u8 Copy_u8PerId){
+    switch(Copy_u8Peripheral){
+    case SYSCTRL_GPIO : SET_BIT(SYSCTRL_RCGCGPIO ,Copy_u8PerId) ; break ;
+    }
+}
+
+void SYSCTRL_voidDisableClock(u8 Copy_u8Peripheral , u8 Copy_u8PerId){
+    switch(Copy_u8Peripheral){
+    case SYSCTRL_GPIO : CLR_BIT(SYSCTRL_RCGCGPIO ,Copy_u8PerId) ; break ;
+    }
+}
 
 
 
