9efd6c1979d1034b6aaedcef7a8577cd4427828a
Muhammad Ali Moutiry <moutiry4@gmail.com>
2020-08-21 10:59:07 +0200

    Add debug folder to master branch

---
+++ Debug/System/02-MCAL/01-SYSCTRL/SYSCTRL_program.d
@@ -0,0 +1,20 @@
+System/02-MCAL/01-SYSCTRL/SYSCTRL_program.o: \
+ ../System/02-MCAL/01-SYSCTRL/SYSCTRL_program.c \
+ ../System/02-MCAL/01-SYSCTRL/SYSCTRL_interface.h \
+ D:/Muhammad_Ali/Courses/IMT/arm/ARM_WS/TivaSDK/System/01-LIB/01-STD_TYPES/STD_TYPES.h \
+ D:/Muhammad_Ali/Courses/IMT/arm/ARM_WS/TivaSDK/System/01-LIB/02-BIT_MATH/BIT_MATH.h \
+ ../System/02-MCAL/01-SYSCTRL/SYSCTRL_reg.h \
+ ../System/02-MCAL/01-SYSCTRL/SYSCTRL_private.h \
+ ../System/02-MCAL/01-SYSCTRL/SYSCTRL_config.h
+
+../System/02-MCAL/01-SYSCTRL/SYSCTRL_interface.h:
+
+D:/Muhammad_Ali/Courses/IMT/arm/ARM_WS/TivaSDK/System/01-LIB/01-STD_TYPES/STD_TYPES.h:
+
+D:/Muhammad_Ali/Courses/IMT/arm/ARM_WS/TivaSDK/System/01-LIB/02-BIT_MATH/BIT_MATH.h:
+
+../System/02-MCAL/01-SYSCTRL/SYSCTRL_reg.h:
+
+../System/02-MCAL/01-SYSCTRL/SYSCTRL_private.h:
+
+../System/02-MCAL/01-SYSCTRL/SYSCTRL_config.h:
---
+++ Debug/main.d
@@ -0,0 +1,19 @@
+main.o: ../main.c \
+ D:/Muhammad_Ali/Courses/IMT/arm/ARM_WS/TivaSDK/System/01-LIB/01-STD_TYPES/STD_TYPES.h \
+ D:/Muhammad_Ali/Courses/IMT/arm/ARM_WS/TivaSDK/System/02-MCAL/01-SYSCTRL/SYSCTRL_interface.h \
+ D:/Muhammad_Ali/Courses/IMT/arm/ARM_WS/TivaSDK/System/01-LIB/02-BIT_MATH/BIT_MATH.h \
+ D:/Muhammad_Ali/Courses/IMT/arm/ARM_WS/TivaSDK/System/02-MCAL/01-SYSCTRL/SYSCTRL_reg.h \
+ D:/Muhammad_Ali/Courses/IMT/arm/ARM_WS/TivaSDK/System/02-MCAL/01-SYSCTRL/SYSCTRL_private.h \
+ D:/Muhammad_Ali/Courses/IMT/arm/ARM_WS/TivaSDK/System/02-MCAL/01-SYSCTRL/SYSCTRL_config.h
+
+D:/Muhammad_Ali/Courses/IMT/arm/ARM_WS/TivaSDK/System/01-LIB/01-STD_TYPES/STD_TYPES.h:
+
+D:/Muhammad_Ali/Courses/IMT/arm/ARM_WS/TivaSDK/System/02-MCAL/01-SYSCTRL/SYSCTRL_interface.h:
+
+D:/Muhammad_Ali/Courses/IMT/arm/ARM_WS/TivaSDK/System/01-LIB/02-BIT_MATH/BIT_MATH.h:
+
+D:/Muhammad_Ali/Courses/IMT/arm/ARM_WS/TivaSDK/System/02-MCAL/01-SYSCTRL/SYSCTRL_reg.h:
+
+D:/Muhammad_Ali/Courses/IMT/arm/ARM_WS/TivaSDK/System/02-MCAL/01-SYSCTRL/SYSCTRL_private.h:
+
+D:/Muhammad_Ali/Courses/IMT/arm/ARM_WS/TivaSDK/System/02-MCAL/01-SYSCTRL/SYSCTRL_config.h:
